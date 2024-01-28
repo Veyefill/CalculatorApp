47859e75e49b067bdcc7ef0d0c4699f1d63b169e
Veyefill <nojusklimov@gmail.com>
2024-01-29 01:17:58 +0200

    Calculator React App - exercise finished

--- App.js
+++ App.js
@@ -1,12 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+
+  const [firstNum, setFirstNum] = useState('');
+  const [secondNum, setSecondNum] = useState('');
+  const [result, setResult] = useState('');
+  const Add = () => {
+    const AddOne = parseFloat(firstNum);
+    const AddTwo = parseFloat(secondNum);
+    setResult(AddOne + AddTwo);
+  }
+  const Subtract = () => {
+    const SubOne = parseFloat(firstNum);
+    const SubTwo = parseFloat(secondNum);
+    setResult(SubOne - SubTwo);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <Text style={{ fontWeight: "bold" }}>Result: {result}</Text>
+      <TextInput style={{ width: 300, borderColor: 'black', borderWidth: 1 }} onChangeText={firstNum => setFirstNum(firstNum)} value={firstNum} keyboardType="numeric"></TextInput>
+      <TextInput style={{ width: 300, borderColor: 'black', borderWidth: 1 }} onChangeText={secondNum => setSecondNum(secondNum)} value={secondNum} keyboardType="numeric"></TextInput>
+      <View style={{ flexDirection: 'row' }}>
+        <Button onPress={Add} title="+"></Button>
+        <View style={{ marginLeft: 10 }}>
+          <Button onPress={Subtract} title="-"></Button>
+        </View>
+      </View>
+    </View >
   );
 }
 
